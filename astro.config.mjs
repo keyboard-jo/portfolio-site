b2d145d5b55299d3717605b41bae8d329703354a
Justin Kennard <65385473+keyboard-jo@users.noreply.github.com>
2026-01-05 16:45:39 +0700

    move assets to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,15 @@ import alpinejs from '@astrojs/alpinejs';
 
 import mdx from '@astrojs/mdx';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   // This wires up the CSS and JS engines
   integrations: [tailwind(), alpinejs(), mdx()],
+
+  image: {
+    domains: ['assets.happymediastrategy.xyz'],
+  },
+
+  adapter: cloudflare(),
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -2,17 +2,15 @@ import { defineCollection, z } from 'astro:content';
 
 const postsCollection = defineCollection({
   type: 'content',
-  // We destructure 'image' from the context to use it for local file validation
-  schema: ({ image }) => z.object({
+  schema: z.object({
     id: z.string(),
     permalink: z.string(),
     title: z.string().max(60),
     subtitle: z.string(),
-    date: z.coerce.date(), // 'coerce' helps turn string dates "2025-01-01" into JS Date objects
-    
-    // This helper validates that the local image exists relative to the .mdx file
-    coverImage: image(), 
-    
+    date: z.coerce.date(), 
+    coverImage: z.string(), 
+    coverWidth: z.number().default(1200),
+    coverHeight: z.number().default(1200),
     tags: z.array(z.string()).optional()
   }),
 });
